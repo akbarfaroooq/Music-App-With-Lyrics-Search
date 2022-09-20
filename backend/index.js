29c7e7bcf628a7bb10ff493f567ad80e5301f2a1
akbarfaroooq <akbarfarooq786421@gmail.com>
2022-09-20 15:15:20 +0530

    ..

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+// importing express
+const express = require('express');
+const userRouter = require('./routers/userRouter');
+const cors = require('cors');
+
+// initiliazing express
+const app = express();
+
+const port = 5000;
+
+// middleware
+// this will parse JSON data to javaScript
+app.use(express.json());
+app.use(cors( { origin : [ 'http://localhost:3000' ] } ));
+
+app.use('/user', userRouter);
+
+// route
+app.get( '/', (req, res) => {
+    res.send('Response from express');
+})
+
+// route or endpoint
+app.get( '/home', (req, res) => {
+    res.send('Response from home');
+})
+
+app.listen( port, () => {
+    console.log('server has started');
+});
